c0c9a63dc2bb2021b096dc174c078f30357618a2
Q4kK <quantumstar4k@gmail.com>
2023-06-27 13:33:56 -0500

    WIP: Fixing nodejs modules and fetch

--- HELMET/src/main.ts
+++ HELMET/src/main.ts
@@ -1,16 +1,16 @@
+import * as fs from 'fs'
+const fetch = await import('node-fetch')
 
+const headers = new Headers();
 
-var myHeaders = new Headers();
-myHeaders.append("x-api-key", FileSystemVar.readFile ("../KEY.txt") );
+headers.append('x-api-key', fs.readFileSync('../Bungie_Key.txt').toString());
 
-var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  };
-
-fetch("https://www.bungie.net/Platform/Content/Rss/NewsArticles/0", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-}
+var myHeaders = {
+     method: 'GET',
+     headers: headers,
+     redirect: 'follow'
+   };
+fetch('https://www.bungie.net/Platform/Content/Rss/NewsArticles/0')
+   .then(response => response.text())
+   .then(result => console.log(result))
+   .catch(error => console.log('error', error));
