67fdcb42b8f0b84ec5b4f5a1322e2dcf842b4cbd
Q4kK <quantumstar4k@gmail.com>
2023-06-28 13:10:51 -0500

    Feat(ts): introduced output to `main.ts`

--- HELMET/src/main.ts
+++ HELMET/src/main.ts
@@ -1,16 +1,37 @@
-import * as fs from 'fs'
-const fetch = await import('node-fetch')
+import fs = require('fs');
+import fetch, { Headers } from 'node-fetch'
 
-const headers = new Headers();
+// fetch('https://www.bungie.net/Platform/Content/Rss/NewsArticles/0', {
+//   method: 'GET',
+//   headers: myheaders,
+//   redirect: 'follow'
+// })
+//   .then(response => response.text())
+//   .then(result => JSON.parse(result))
+//   .catch(error => console.log('error', error));
 
-headers.append('x-api-key', fs.readFileSync('../Bungie_Key.txt').toString());
+async function get_json(headers) {
+  const response = await fetch('https://www.bungie.net/Platform/Content/Rss/NewsArticles/0', {
+    headers,
+    method: 'GET',
+    redirect: 'follow'
+  });
+  const result = await response.text();
+  return JSON.parse(result);
+}
 
-var myHeaders = {
-     method: 'GET',
-     headers: headers,
-     redirect: 'follow'
-   };
-fetch('https://www.bungie.net/Platform/Content/Rss/NewsArticles/0')
-   .then(response => response.text())
-   .then(result => console.log(result))
-   .catch(error => console.log('error', error));
+function newsloop(News) {
+  return News.filter(item => item["Title"].startsWith("This Week"));
+  // for (var i = 0; i < twab_news.length; i++) {
+  //   return twab_news.filter(item => item["Link"])
+  //   }
+  }
+
+async function main() {
+  let headers = new Headers();
+  headers.append("x-api-key", fs.readFileSync('../Bungie_Key.txt').toString().trim());
+  const bungie_response = await get_json(headers);
+  console.log(newsloop(bungie_response["Response"]["NewsArticles"]))
+}
+
+main();
