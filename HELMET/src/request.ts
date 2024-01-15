60b123471d2b841bd997e400ca0c02ae54963f33
q4kk <quantumstar4k@gmail.com>
2024-01-15 16:55:37 -0600

    Feat: revamping html

--- HELMET/src/request.ts
+++ HELMET/src/request.ts
@@ -15,6 +15,11 @@ async function get_json(headers) {
   return JSON.parse(result);
 }
 
+function destiny_update(News: postInfo[]): string {
+  let update = News.filter(item => item.Title.startsWith("Destiny"));
+  return BUNGIE_URL + update[0].Link;
+}
+
 function latest_twab(News: postInfo[]): string {
   let twab = News.filter(item => item.Title.startsWith("This Week"));
   return BUNGIE_URL + twab[0].Link;
@@ -24,7 +29,8 @@ async function main() {
   let headers = new Headers();
   headers.append("x-api-key", fs.readFileSync('./Bungie_Key.txt').toString().trim());
   const bungie_response = await get_json(headers);
-  console.log(latest_twab(bungie_response["Response"]["NewsArticles"]));
+  document.body.innerHTML = "<h1>" + (latest_twab(bungie_response["Response"]["NewsArticles"])) + "</h1>";
+  document.body.innerHTML = "<h1>" + (destiny_update(bungie_response["Response"]["NewsArticles"])) + "</h1>";
 }
 
 main();
